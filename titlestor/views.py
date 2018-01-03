cdf66e20d3732d36ac1845244b6a1d91e413ec59
gmartinez31 <bhgustavo13@yahoo.com>
2018-01-03 09:55:16 -0600

    setting up templates

--- titlestor/views.py
+++ titlestor/views.py
@@ -6,3 +6,9 @@ def index(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def confirmation(request):
+    return render(request, 'confirmation.html')
+
+def error(request):
+    return render(request, 'error.html')
